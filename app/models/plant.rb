586d52d9108a98e5aa141704f2e366d62ed85b28
dancunrandy <dancunrandy@gmail.com>
2023-03-18 08:29:47 +0300

    completed assignment

---
+++ app/models/plant.rb
@@ -0,0 +1,6 @@
+class Plant < ApplicationRecord
+    validates :name, presence: true
+    validates :image, presence: true
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  end
+  
